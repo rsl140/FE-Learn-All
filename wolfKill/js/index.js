d48b4fcd1c576d4298c3bb586b24a47838039460
rsl140 <rsl140@qq.com>
2017-08-16 21:48:53 +0800

    修正手机版背景，更新新的背景图片

--- wolfKill/js/index.js
+++ wolfKill/js/index.js
@@ -3,6 +3,18 @@ $(function(){
 	$("#left-card").css('height',contrntHeight+"px");
 	$("#left-card ul li").css('height',contrntHeight/6+"px");
 	$("#right-card ul li").css('height',contrntHeight/6+"px");
-	$("#chat-display").css('height',contrntHeight/1.4+"px");
-	$("#chat-input").css('height',contrntHeight-(contrntHeight/1.4)+"px");
+	$("#chat-display").css('height',contrntHeight+"px");
+
+	$(".li-card").addClass('animated flipInY').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass('animated flipInY');
+    });
+
+	$(".li-card").click(function(){
+		$(this).removeClass('animated flipInY').addClass('animated flipInY bc').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      	$(this).removeClass('animated flipInY');
+    });
+		
+
+	})
+
 })
