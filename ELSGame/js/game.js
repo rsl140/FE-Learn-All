c09180572a97eea1fb0bbc05c01aac2149f9a58e
rsl140 <rsl140@qq.com>
2017-09-04 15:24:14 +0800

    完善计时，计分功能

--- ELSGame/js/game.js
+++ ELSGame/js/game.js
@@ -2,6 +2,11 @@ var Game = function() {
 	// dom元素
 	var gameDiv;
 	var nextDiv;
+	var timeDiv;
+	var scoreDiv;
+	var resultDiv;
+	// 分数
+	var score = 0;
 	// 游戏矩阵
 	var gameData = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -161,17 +166,107 @@ var Game = function() {
 		}
 		
 	}
-
+	// 当方块掉落到底部时结束
+	var fixed = function() {
+		for (var i = 0; i < cur.data.length; i++) {
+			for(var j = 0;j<cur.data[0].length; j++) {
+				if(check(cur.origin, i ,j)) {
+					if(gameData[cur.origin.x + i][cur.origin.y + j] == 2) {
+						gameData[cur.origin.x + i][cur.origin.y + j] = 1;
+					}
+				}
+			}
+		}
+		refreshDiv(gameData, gameDivs);
+	}
+	// 消除
+	var checkClear = function() {
+		var line = 0;
+		for (var i = gameData.length - 1; i >= 0; i--) {
+			var clear = true;
+			for(var j = 0; j<gameData[0].length; j++) {
+				if(gameData[i][j] != 1) {
+					clear = false;
+					break;
+				}
+			}
+			if(clear) {
+				line = line + 1;
+				for(var m = i; m > 0; m--) {
+					for(var n = 0; n<gameData[0].length; n++) {
+						gameData[m][n] = gameData[m-1][n];
+					}
+				}
+				for(var n = 0; n<gameData[0].length; n++) {
+						gameData[0][n] = 0;
+				}
+				i++;
+			}
+		}
+		return line;
+	}
+	// 检查游戏结束
+	var checkGameOver = function() {
+		var gameOver = false;
+		for(var i = 0; i<gameData[0].length; i++) {
+			if(gameData[1][i] == 1) {
+				gameOver = true;
+			}
+		}
+		return gameOver;
+	}
+	// 刷新下一个方块
+	var performNext = function(type, dir) {
+		cur = next;
+		setData();
+		next = SquareFactory.prototype.make(type, dir);
+		refreshDiv(gameData, gameDivs);
+		refreshDiv(next.data, nextDivs);
+	}
+	// 设置时间
+	var setTime = function(time) {
+		timeDiv.innerHTML = time;
+	}
+	// 分数
+	var addScore = function(line) {
+		var s = 0;
+		switch(line) {
+			case 1:
+				s = 10;
+				break;
+			case 2:
+				s =30
+				break;
+			case 3:
+				s = 60;
+				break;
+			case 4:
+				s =100
+				break;
+			default:
+				break;
+		}
+		score = score + s;
+		scoreDiv.innerHTML = score;
+	}
+	// 游戏结束
+	var gameOver = function(win) {
+		if(win) {
+			resultDiv.innerHTML = 'You Win';
+		} else {
+			resultDiv.innerHTML = 'Game Over';
+		}
+	}
 	// 初始化
-	var init = function(doms) {
+	var init = function(doms, type , dir) {
 		gameDiv = doms.gameDiv;
 		nextDiv = doms.nextDiv;
-		cur = SquareFactory.prototype.make(2, 2);
-		next = SquareFactory.prototype.make(3, 3);
+		timeDiv = doms.timeDiv;
+		scoreDiv = doms.scoreDiv;
+		resultDiv = doms.resultDiv;
+		next = SquareFactory.prototype.make(type , dir);
 		initDiv(gameDiv, gameData, gameDivs);//游戏主体
 		initDiv(nextDiv, next.data, nextDivs);//下一个
-		setData();
-		refreshDiv(gameData, gameDivs);
 		refreshDiv(next.data, nextDivs);
 	}
 
@@ -182,4 +277,11 @@ var Game = function() {
 	this.right = right;
 	this.rotate = rotate;
 	this.fall = function() {while(down());}
+	this.fixed = fixed;
+	this.performNext = performNext;
+	this.checkClear = checkClear;
+	this.checkGameOver = checkGameOver;
+	this.setTime = setTime;
+	this.addScore = addScore;
+	this.gameOver = gameOver;
 }
--- ELSGame/js/local.js
+++ ELSGame/js/local.js
@@ -1,6 +1,14 @@
 var Local = function() {
 	// 游戏对象
 	var game;
+	// 时间间隔
+	var INTERVAL = 200;
+	// 定时器
+	var timer = null;
+	// 时间计数器
+	var timeCount = 0;
+	// 时间
+	var time = 0;
 	//绑定键盘事件
 	var bindKeyEvent = function() {
 		document.onkeydown = function(e) {
@@ -17,15 +25,65 @@ var Local = function() {
 			}
 		}
 	}
+	// 移动
+	var move = function() {
+		timeFunc();
+		if(!game.down()) {
+			game.fixed();
+			var line = game.checkClear();
+			if(line) {
+				game.addScore(line);
+			}
+			var gameOver = game.checkGameOver();
+			if(gameOver) {
+				game.gameOver(false);
+				stop();
+			}else{
+				game.performNext(generateType(), generateDir());
+			}
+		}
+	}
+	// 计时函数
+	var timeFunc = function() {
+		timeCount = timeCount + 1;
+		if(timeCount == 5) {
+			timeCount = 0;
+			time = time + 1;
+			game.setTime(time);
+		}
+	}
+	// 随机生成一个方块种类
+	var generateType = function() {
+		return Math.ceil(Math.random() * 7) - 1;
+	}
+
+	// 随机生成一个旋转样式
+	var generateDir = function() {
+		return Math.ceil(Math.random() * 4) - 1;
+	}
 	// 开始
 	var start = function() {
 		var doms = {
 			gameDiv: document.getElementById('game'),
-			nextDiv: document.getElementById('next')
+			nextDiv: document.getElementById('next'),
+			timeDiv: document.getElementById('time'),
+			scoreDiv: document.getElementById('score'),
+			resultDiv: document.getElementById('gameover')
+
 		}
 		game = new Game();
-		game.init(doms);
+		game.init(doms, generateType(), generateDir());
 		bindKeyEvent();
+		game.performNext(generateType(), generateDir());
+		timer = setInterval(move, INTERVAL);
+	}
+	// 结束
+	var stop = function() {
+		if(timer) {
+			clearInterval(timer);
+			timer = null;
+		}
+		document.onkeydown = null;
 	}
 	// 导出API
 	this.start = start;
