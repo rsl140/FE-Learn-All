63c8ad1def4fa020eca1b4b23799949a07ac814f
rsl140 <rsl140@qq.com>
2020-05-13 15:17:13 +0800

    feat: leetcode 21

---
+++ leetcode/21.合并两个有序链表.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (56.39%)
+ * Likes:    1032
+ * Dislikes: 0
+ * Total Accepted:    261.5K
+ * Total Submissions: 419.8K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  var pre = {
+    val: -1,
+    next: null
+  },
+  // 存储当前值
+  cur = pre
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      cur.next = l2
+      l2 = l2.next
+    } else {
+      cur.next = l1
+      l1 = l1.next
+    }
+    cur = cur.next
+  }
+  cur.next = l1 || l2
+  return pre.next
+};
+// @lc code=end
+
